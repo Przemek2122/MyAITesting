a7de0aaffd54cfe4d083b13bd7fed9b25d72b7bc
Przemek <przemek12wiewiora@gmail.com>
2025-09-12 21:06:11 +0200

    Initial commit

---
+++ Project/SourcesTest/MainTestFile.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "Public/Project.h"
+
+int main(int argc, char* argv[])
+{
+	
+}
+
+// Sample test
+TEST(Test, BasicTest) {
+    // Your test code here
+    EXPECT_EQ(1 + 1, 2);
+}
